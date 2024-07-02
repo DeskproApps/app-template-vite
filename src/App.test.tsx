64537e8f91a405e5d01d5e22fbaec1d6eb51ca9a
ilia makarov <ilia.makarov@me.com>
2024-07-02 11:48:38 +0300

    Upgraded app-sdk, removed iframe-resizer, rewrote tests with app-testing-utils

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { cleanup } from "@testing-library/react";
-import { render } from "../testing";
+import { render } from "@deskpro/app-testing-utils";
 import { App } from "./App";
 
 jest.mock("./pages/Main/usePosts", () => ({
