020539204c91d77b863b7c9ddb3f279958ea455e
Paul Hutchinson <paulhappyhutchinson@protonmail.ch>
2025-05-16 09:32:13 +0000

    CI-CD upgrades

--- bin/bumpManifestVer.js
+++ bin/bumpManifestVer.js
@@ -1,36 +1,28 @@
 const fs = require("fs");
 
-const bumpSemanticVersion = (versionString, bumpType = "patch") => {
-  let [major, minor, patch] = versionString.split(".");
+function bumpSemanticVersion(versionString, labels) {
+  let [major, minor, patch] = (versionString ?? '0.0.0').split(".");
 
-  switch (bumpType) {
-    case "major":
-      major = parseInt(major) + 1;
-      minor = 0;
-      patch = 0;
-      break;
-
-    case "minor":
-      minor = parseInt(minor) + 1;
-      patch = 0;
-      break;
-
-    case "patch":
-      patch = parseInt(patch) + 1;
-      break;
-
-    default:
-      break;
+  if (labels?.includes("major")) {
+    major = parseInt(major) + 1;
+    minor = 0;
+    patch = 0;
+  } else if (labels?.includes("minor")) {
+    minor = parseInt(minor) + 1;
+    patch = 0;
+  } else {
+    patch = parseInt(patch) + 1;
   }
 
   return `${major}.${minor}.${patch}`;
 };
 
 const packageJson = JSON.parse(fs.readFileSync("./manifest.json", "utf8"));
-//1
+
 packageJson.version = bumpSemanticVersion(
-  process.argv[3] ? process.argv[3] : packageJson.version,
-  process.argv[2]
+  process.argv[3],
+  process.argv[2],
 );
 
-fs.writeFileSync("./manifest.json", JSON.stringify(packageJson));
+fs.writeFileSync("./manifest.json", JSON.stringify(packageJson, null, 2));
+console.log(packageJson.version);
